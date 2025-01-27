ebb6c519c480cdc48dc64d8467f03505afd51497
fatiha <156305510+DevTiyah@users.noreply.github.com>
2025-01-27 14:04:09 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,12 +17,6 @@ export default function Home() {
     <HeroSection />
     <ProjectsSection />
     <div className="magicpattern inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_80%,transparent)]">
-     {/* <div className='absolute h-full inset-0 -z-30 opacity-10' style={{
-        backgroundImage: `url(${grainImage.src})`,
-        // backgroundAttachment: 'fixed',
-        // backgroundRepeat: 'repeat-y',
-      }}>;
-      </div> */}
       <TapeSection/>
       <TestimonialsSection/>
       
