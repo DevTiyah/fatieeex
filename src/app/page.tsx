321cd2ba0b2049590569962b18f3582a42f5ab06
DevTiyah <devtiyah@gmail.com>
2025-01-27 13:57:53 +0100

    bug fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import { TestimonialsSection } from "@/sections/Testimonials";
 import { AboutSection } from "@/sections/About";
 import { ContactSection } from "@/sections/Contact";
 import { Footer } from "@/sections/Footer";
-import grainImage from '@/assets/images/grain.jpg'
 
 
 export default function Home() {
